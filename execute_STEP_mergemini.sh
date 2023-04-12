f4926299fe3001102bea91c72478e667da6caf49
Brandon Chiarito <bchiarito@gmail.com>
2023-04-11 20:04:50 -0500

    remove 10 event cap in mergedmini step

--- execute_STEP_mergemini.sh
+++ execute_STEP_mergemini.sh
@@ -13,5 +13,9 @@ cd CMSSW_10_6_20/src
 eval `scramv1 runtime -sh`
 cd ../..
 
-cmsRun merge_cfg.py input=$1 outputFile=output.root maxEvents=10
-xrdcp --nopbar output_numEvent10.root $2/$3.root
+cmsRun merge_cfg.py input=$1 outputFile=output.root maxEvents=-1
+echo ""
+echo "finished cmsRun"
+ls
+xrdcp --nopbar output.root $2/$3.root
+rm *.root
